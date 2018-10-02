721ac38bfcb933dfc5462f937af1e5f2ce415a7b
Emilijus <slibinas2012@gmail.com>
2018-10-02 15:00:00 +0300

    Started supernovas

---
+++ Assets/Supernova.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Supernova : MonoBehaviour
+{
+    [SerializeField] float expandRate;
+    [SerializeField] float maxRadius;
+    [SerializeField] float starLivesToRemove = 5f;
+    [SerializeField] float waitTimeBetweenLifeRemove = 0.3f;
+
+    ParticleSystem supernovaPS;
+
+	void Start ()
+    {
+        supernovaPS = GetComponentInChildren<ParticleSystem>();		
+	}
+	
+	void Update ()
+    {
+        var supernovaShape = supernovaPS.shape;
+        supernovaShape.radius += expandRate * Time.deltaTime;
+        if(supernovaShape.radius > maxRadius)
+        {
+            Explode();
+        }
+	}
+
+    void Explode()
+    {
+        foreach(Star star in FindObjectsOfType<Star>())
+        {
+            if(Vector2.Distance(star.transform.position, transform.position) <= maxRadius)
+            {
+                StartCoroutine(RemoveStarLife(star));
+            }
+        }
+        Destroy(supernovaPS);
+        Destroy(gameObject, starLivesToRemove * waitTimeBetweenLifeRemove);
+    }
+
+    IEnumerator RemoveStarLife(Star star)
+    {
+        for (int i = 0; i < starLivesToRemove; i++)
+        {
+            star.RemoveAmmo();
+            yield return new WaitForSeconds(waitTimeBetweenLifeRemove);
+        }
+    }
+}
---
+++ Assets/SupernovaSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupernovaSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] comets;
+    [SerializeField] float minSpawnTime;
+    [SerializeField] float maxSpawnTime;
+
+    bool playing = true;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

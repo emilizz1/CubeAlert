d979066d4aca3d82a600a1a71dd3dc51301ef78e
Emilijus <slibinas2012@gmail.com>
2018-11-06 21:12:26 +0200

    Upgrades now fade and rotate

--- Assets/Upgrades/UpgradeController.cs
+++ Assets/Upgrades/UpgradeController.cs
@@ -4,7 +4,55 @@ using UnityEngine;
 
 public class UpgradeController : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float fadeSpeed = 1f;
+
+    bool alive = true;
+    bool fading = true;
+
     int extraTime, extraDamage, extraTaps;
+    float rotation;
+    SpriteRenderer mySpriteRenderer;
+
+    void Start()
+    {
+        transform.Rotate(new Vector3(0f, 0f, Random.Range(0f, 360f)));
+        rotation = Random.Range(-rotationSpeed, rotationSpeed);
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        StartCoroutine(FadeTimer());
+        Fade();
+        Rotate();
+    }
+
+    void Fade()
+    {
+        if (fading)
+        {
+            mySpriteRenderer.color = new Color(1f, 1f, 1f, mySpriteRenderer.color.a - (Time.deltaTime * fadeSpeed));
+        }
+        else
+        {
+            mySpriteRenderer.color = new Color(1f, 1f, 1f, mySpriteRenderer.color.a + (Time.deltaTime * fadeSpeed));
+        }
+    }
+
+    IEnumerator FadeTimer()
+    {
+        while (alive)
+        {
+            yield return new WaitForSeconds(Random.Range(1.5f, 3f));
+            fading = !fading;
+        }
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * rotation));
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {

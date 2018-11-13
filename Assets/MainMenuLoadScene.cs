c683ea8f4a190db955b0e6b447c5800c4d7c05bd
Emilijus <slibinas2012@gmail.com>
2018-11-13 21:34:28 +0200

    Main screen now scales better. Tutorial is played only once.

---
+++ Assets/MainMenuLoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoadScene : MonoBehaviour
+{
+    [SerializeField] int tutorialSceneNumber = 2;
+    [SerializeField] int mainGameSceneNumber = 1;
+
+    public void mLoadScene()
+    {
+        if (FindObjectOfType<TutorialCompleted>().GetIsTutorialCompleted())
+        {
+            SceneManager.LoadScene(mainGameSceneNumber);
+        }
+        else
+        {
+            SceneManager.LoadScene(mainGameSceneNumber);
+        }
+    }
+}
--- Assets/Scenes/TutorialGuide.cs
+++ Assets/Scenes/TutorialGuide.cs
@@ -43,6 +43,7 @@ public class TutorialGuide : MonoBehaviour
         {
             arrow.SetActive(false);
             FindObjectOfType<EndLevelFlash>().EndLevel();
+            FindObjectOfType<TutorialCompleted>().TutorialFinished();
             Invoke("StartLoadingScene", 2f);
         }
     }

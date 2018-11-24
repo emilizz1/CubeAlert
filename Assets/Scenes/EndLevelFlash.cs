c87d51624f4f4de2befcbc05c9a55f902b37f0c7
Emilijus <slibinas2012@gmail.com>
2018-11-24 11:56:31 +0200

    Started creating second tutorial

--- Assets/Scenes/EndLevelFlash.cs
+++ Assets/Scenes/EndLevelFlash.cs
@@ -9,7 +9,6 @@ public class EndLevelFlash : MonoBehaviour
     [SerializeField] GameObject center;
     [SerializeField] GameObject triangle;
     [SerializeField] float triangleMaxSize;
-    [SerializeField] float cameraZoomSpeed = 1f;
 
     bool once = true;
 

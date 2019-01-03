eef0d61e3bc150c59953105e69c3094b9dec6adf
Emilijus <slibinas2012@gmail.com>
2019-01-03 22:05:47 +0200

    Added different tunnel

--- Assets/Background/TunnelRandomizer.cs
+++ Assets/Background/TunnelRandomizer.cs
@@ -8,19 +8,34 @@ public class TunnelRandomizer : MonoBehaviour
     bool playing = true;
 
     TunnelFX2 myTunnel;
-    
-	void Start ()
+    float minColor, maxColor;
+
+    void Start()
     {
         myTunnel = GetComponent<TunnelFX2>();
         RandomizeTunnel();
         StartCoroutine(ChangingBackgroundColors());
         StartCoroutine(ChangingTintColors());
     }
-	
-	void RandomizeTunnel()
+
+    void RandomizeTunnel()
     {
+        switch (Random.Range(0, 2))
+        {
+            case (0):
+                myTunnel.preset = TUNNEL_PRESET.SpaceTravel;
+                myTunnel.hyperSpeed = Random.Range(0f, 0.6f);
+                minColor = 0.4f;
+                maxColor = 1f;
+                break;
+            case (1):
+                myTunnel.preset = TUNNEL_PRESET.CloudAscension;
+                myTunnel.hyperSpeed = Random.Range(0f, 0.4f);
+                minColor = 0.6f;
+                maxColor = 1f;
+                break;
+        }
         myTunnel.animationAmplitude = 0.1f;
-        myTunnel.hyperSpeed = Random.Range(0f, 0.6f);
         myTunnel.globalAlpha = Random.Range(0.5f, 0.8f);
         myTunnel.fallOff = Random.Range(0.3f, 1f);
     }
@@ -30,11 +45,11 @@ public class TunnelRandomizer : MonoBehaviour
         switch (Random.Range(0, 3))
         {
             case (0):
-                return new Color(1f, Random.Range(0.4f, 1f), Random.Range(0.4f, 1f));
+                return new Color(1f, Random.Range(minColor, maxColor), Random.Range(minColor, maxColor));
             case (1):
-                return new Color(Random.Range(0.4f, 1f), 1f, Random.Range(0.4f, 1f));
+                return new Color(Random.Range(minColor, maxColor), 1f, Random.Range(minColor, maxColor));
             case (2):
-                return new Color(Random.Range(0.4f, 1f), Random.Range(0.4f, 1f), 1f);
+                return new Color(Random.Range(minColor, maxColor), Random.Range(minColor, maxColor), 1f);
             case (3):
                 break;
         }

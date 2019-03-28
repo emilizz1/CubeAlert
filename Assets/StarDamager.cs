17be4f949b3b9291df11513715397f9209449a67
Emilijus <slibinas2012@gmail.com>
2019-03-28 12:03:22 +0200

    Started working on 3D objects in challenge level

---
+++ Assets/StarDamager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarDamager : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Star>())
+        {
+            collision.gameObject.GetComponent<Star>().RemoveStarLife(1);
+        }
+    }
+}

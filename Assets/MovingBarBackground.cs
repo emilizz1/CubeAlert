b59883d22e9caa960fe5d815e36f35a7c1c092ec
Emilijus <slibinas2012@gmail.com>
2019-04-11 11:48:51 +0300

    Started working on bar backgrounds

---
+++ Assets/MovingBarBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingBarBackground : MonoBehaviour
+{
+    [SerializeField] GameObject firstImage;
+    [SerializeField] GameObject secondImage;
+
+    float movingSpeed;
+    
+    void Start()
+    {
+        movingSpeed = Random.Range(0.01f, 0.1f);
+    }
+    
+    void Update()
+    {
+        
+    }
+}

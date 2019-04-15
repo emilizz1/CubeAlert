2e4f34b2c1f2ad2047ac201832d2946b0bcaad07
Emilijus <slibinas2012@gmail.com>
2019-04-15 19:39:36 +0300

    Added new grid for placement positions

--- Assets/Beams/BeamSpawner.cs
+++ Assets/Beams/BeamSpawner.cs
@@ -4,43 +4,60 @@ using UnityEngine;
 
 public class BeamSpawner : MonoBehaviour
 {
+    [SerializeField] Vector2[] removedPositions;
     [SerializeField] int objectsToSpawn = 4;
     [SerializeField] GameObject simpleBeam;
     [SerializeField] GameObject crossBeam;
     [SerializeField] GameObject circleBeam;
 
+    int gridType;
     List< Vector3> usedPositions = new List<Vector3>();
     List< Vector3> movedPositions = new List<Vector3>();
 
+    List<Vector2> grid = new List<Vector2>();
+
     void Start()
     {
+        gridType = Random.Range(0, 2);
+        AddGrid();
         while(usedPositions.Count < objectsToSpawn)
         {
             SpawnBeam(Random.Range(0, 3));
         }
     }
 
+    void AddGrid()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int o = 0; o < 9; o++)
+            {
+                grid.Add(new Vector2(i, o));
+            }
+        }
+        foreach(Vector2 pos in removedPositions)
+        {
+            grid.Remove(pos);
+        }
+    }
+
     void SpawnBeam(int beamToSpawn)
     {
         Vector3 spawnLocation = GetSpawnLocation();
-        if (IsSpawnLocationAllowed(spawnLocation))
+        switch (beamToSpawn)
         {
-            usedPositions.Add(spawnLocation);
-            switch (beamToSpawn)
-            {
-                case (0):
-                    var spawnedBeam = Instantiate(simpleBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
-                    spawnedBeam.GetComponent<BeamLine>().Rotate(GetRandomBool());
-                    spawnedBeam.GetComponent<BeamLine>().Shrinking(GetRandomBool());
-                    spawnedBeam.GetComponent<BeamLine>().Moving(GetRandomBool(), GetMovedPosition(spawnedBeam.transform.position));
-                    break;
-                case (1):
-                    Instantiate(circleBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
-                    break;
-                case (2):
-                    Instantiate(crossBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
-                    break;
-            }
+            case (0):
+                var spawnedBeam = Instantiate(simpleBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
+                spawnedBeam.GetComponent<BeamLine>().Rotate(Random.value < 0.5f);
+                spawnedBeam.GetComponent<BeamLine>().Shrinking(Random.value < 0.5f);
+                spawnedBeam.GetComponent<BeamLine>().Moving(Random.value < 0.5f, GetMovedPosition(spawnedBeam.transform.position));
+                break;
+            case (1):
+                Instantiate(circleBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
+                break;
+            case (2):
+                Instantiate(crossBeam, spawnLocation, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), transform);
+                break;
         }
     }
 
@@ -49,30 +66,30 @@ public class BeamSpawner : MonoBehaviour
         switch(Random.Range(0, 4))
         {
             case (0):
-                if (movingFrom.x + 15.3f < 20f)
+                if (movingFrom.x + 9.2f < 20f)
                 {
-                    movingFrom.x += 15.3f;
+                    movingFrom.x += 9.2f;
                     movedPositions.Add(movingFrom);
                 }
                 break;
             case (1):
-                if (movingFrom.x - 15.3f > -20f)
+                if (movingFrom.x - 9.2f > -20f)
                 {
-                    movingFrom.x -= 15.3f;
+                    movingFrom.x -= 9.2f;
                     movedPositions.Add(movingFrom);
                 }
                 break;
             case (2):
-                if (movingFrom.y - 11.5f > -30f)
+                if (movingFrom.y - 7.6f > -30f)
                 {
-                    movingFrom.y -= 11.5f;
+                    movingFrom.y -= 7.6f;
                     movedPositions.Add(movingFrom);
                 }
                 break;
             case (3):
-                if (movingFrom.y + 11.5f > 30f)
+                if (movingFrom.y + 7.6f > 30f)
                 {
-                    movingFrom.y += 11.5f;
+                    movingFrom.y += 7.6f;
                     movedPositions.Add(movingFrom);
                 }
                 break;
@@ -80,37 +97,20 @@ public class BeamSpawner : MonoBehaviour
         return movingFrom;
     }
 
-    bool GetRandomBool()
-    {
-        if(Random.Range(0,2) == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    bool IsSpawnLocationAllowed(Vector3 spawnLoc)
-    {
-        foreach(Vector3 pos in usedPositions)
-        {
-            if((spawnLoc.x == pos.x && spawnLoc.y == pos.y) || (spawnLoc.x + 15.3f == pos.x && spawnLoc.y == pos.y) || (spawnLoc.x - 15.3f == pos.x && spawnLoc.y == pos.y) ||
-                (spawnLoc.x == pos.x && spawnLoc.y + 11.5f == pos.y) || (spawnLoc.x == pos.x && spawnLoc.y - 11.5f == pos.y))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
     Vector3 GetSpawnLocation()
     {
-        Vector3 newLocation = new Vector3();
-        newLocation.x = -15.3f + (15.3f * Random.Range(0, 3));
-        newLocation.y = -28.75f + (11.5f * Random.Range(0, 6));
-        newLocation.z = 0f;
-        return newLocation;
+        while (true)
+        {
+            int gridSpot = Random.Range(0, grid.Count);
+            if ((grid[gridSpot].x + grid[gridSpot].y) % 2 == gridType)
+            {
+                Vector3 newLocation = new Vector3();
+                newLocation.x = (grid[gridSpot].x - 2) * 9.2f;
+                newLocation.y = (grid[gridSpot].y - 4) * 7.6f;
+                newLocation.z = 0f;
+                grid.Remove(new Vector2(grid[gridSpot].x, grid[gridSpot].y));
+                return newLocation;
+            }
+        }
     }
 }
